f5475ef6f47de1fa0c6d1b648c501cad1f0fb086
Henry Ripple <hripple46@gmail.com>
2023-02-27 16:25:47 -0600

    changed General_Info.js to be a functional component; updated other files to reflect this

--- src/components/General_Info.js
+++ src/components/General_Info.js
@@ -1,62 +1,27 @@
-import { Component } from "react";
+import React, { useState } from "react";
 
-export default class GeneralInfo extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      nameInput: "",
-      name: "",
-      phoneNumberInput: null,
-      phoneNumber: null,
-    };
-    this.changeName = this.changeName.bind(this);
-    this.changeNumber = this.changeNumber.bind(this);
-    this.displayGeneralInfo = this.displayGeneralInfo.bind(this);
-  }
-  changeName = (e) => {
-    this.setState({
-      nameInput: e.target.value,
-    });
-  };
-  changeNumber = (e) => {
-    this.setState({
-      phoneNumberInput: e.target.value,
-    });
-  };
-  displayGeneralInfo() {
-    if (this.props.isSubmitted) {
-      return (
-        <div>
-          <h1>{this.state.nameInput}</h1>
-          <h1>{this.state.phoneNumberInput}</h1>
-        </div>
-      );
-    } else {
-      return (
-        <div>
-          <label htmlFor="name">Full Name: </label>
-          <input
-            type="text"
-            name="name"
-            value={this.state.nameInput}
-            onChange={this.changeName}
-          ></input>
-          <label htmlFor="phoneNumber">Phone Number: </label>
-          <input
-            type="number"
-            name="phoneNumber"
-            onChange={this.changeNumber}
-          ></input>
-        </div>
-      );
-    }
-  }
+function GeneralInfo(props) {
+  let [name, changeName] = useState("Placeholder");
 
-  render() {
+  let updateName = (e) => {
+    changeName(e.target.value);
+  };
+
+  if (!props.submissionStatus) {
     return (
       <div>
-        <this.displayGeneralInfo />
+        <label htmlFor="name">Your Name:</label>
+        <input
+          onChange={updateName}
+          type="text"
+          name="name"
+          value={name}
+        ></input>
       </div>
     );
+  } else {
+    return <h1>{name}</h1>;
   }
 }
+
+export default GeneralInfo;
